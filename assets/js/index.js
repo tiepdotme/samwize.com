bdc26ca8c01e33a70c07d134610e7722689e40b4
Junda <junda@just2us.com>
2016-06-30 17:38:52 +0800

    Open all links in new tab

--- assets/js/index.js
+++ assets/js/index.js
@@ -27,3 +27,8 @@
     });
 
 }(jQuery));
+
+// All external links open in new tab
+$(document.links).filter(function() {
+    return this.hostname != window.location.hostname;
+}).attr('target', '_blank');
